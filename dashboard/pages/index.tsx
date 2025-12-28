c329d9290e7f2944a0c8d0acce6ec85d0ccad9f2
dentucotv <trumangabrielmoreira90@gmail.com>
2025-12-28 18:41:46 -0300

    Add files via upload

---
+++ dashboard/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      background: '#020617',
+      color: 'white',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h1>CFL Dashboard</h1>
+    </main>
+  )
+}
